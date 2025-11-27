2b6ebe3ba8fcf5f55a47ca4c40d1900c14429a3c
amit from linux machine <amitthakur10sep@gmail.com>
2025-11-28 01:04:27 +0530

    movie project

---
+++ movie-project/app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const safed = () => {
+  return (
+    <View>
+      <Text>safed</Text>
+    </View>
+  )
+}
+
+export default safed
